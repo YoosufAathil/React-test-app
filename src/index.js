9c4cbe15361db11c6db8a42f50ddd6f9e2985f17
Yoosuf Aathil <yoosufaath007@gmail.com>
2024-10-18 15:42:42 +0530

    Refactor code to separate App component into its own file

--- src/index.js
+++ src/index.js
@@ -1,10 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from "./App"
 
-function App(){
-  return (
-    <h1>Hello World !! </h1>
-  )
-}
 
 ReactDOM.render(<App/>,document.getElementById('root'));
